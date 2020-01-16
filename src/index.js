5d4f5884278f4fe15cc722ae3ecb9522a641c064
Brian Strauch <bstrauch24@gmail.com>
2020-01-15 18:04:21 -0600

    Removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import logo from './logo.svg';
 import './index.css';
-import App from './App';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
