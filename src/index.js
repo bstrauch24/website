bf08d9f7fa07d4692f84a977631e59262885531f
Brian Strauch <bstrauch24@gmail.com>
2020-03-05 17:52:37 -0600

    Transitioned to blog

--- src/index.js
+++ src/index.js
@@ -1,69 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Ball from './components/Ball';
 import './style.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      w: window.innerWidth,
-      h: window.innerHeight,
-      ball: {x: 100, y: 200, vx: -1, vy: 1, r: 50}
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-    this.animate();
-  }
-
   render() {
-    const { ball } = this.state;
-
     return (
       <div className="App">
-        <h1>Hi, I'm<br />Brian.</h1>
-        <Ball {...ball} />
+        <div className="landing">
+          <h1>
+            Hi, I'm<br />
+            Brian.
+          </h1>
+        </div>
+
+        <div className="blog">
+          <h2>Blog</h2>
+          <p>Coming soon!</p>
+        </div>
       </div>
     );
-  }
-
-  handleResize = () => {
-    this.setState({
-      w: window.innerWidth,
-      h: window.innerHeight
-    });
-  }
-
-  animate = () => {
-    requestAnimationFrame(this.animate);
-
-    const { ball } = this.state;
-
-    if (this.didCollideOnX(ball)) {
-      ball.vx = -ball.vx;
-    }
-    if (this.didCollideOnY(ball)) {
-      ball.vy = -ball.vy;
-    }
-
-    ball.x += ball.vx;
-    ball.y += ball.vy;
-
-    this.setState({ ball });
-  }
-
-  didCollideOnX = ball => {
-    const { w } = this.state;
-    return ball.x + ball.vx - ball.r < 0 || ball.x + ball.vx + ball.r >= w;
-  }
-
-  didCollideOnY = ball => {
-    const { h } = this.state;
-    return ball.y + ball.vy - ball.r < 0 || ball.y + ball.vy + ball.r >= h;
   }
 }
 
