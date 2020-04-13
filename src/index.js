551a859057af90643eaad75ef825ea542be2ab6f
Brian Strauch <bstrauch24@gmail.com>
2020-04-12 23:12:11 -0500

    Package update

--- src/index.js
+++ src/index.js
@@ -7,17 +7,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="landing">
-          <h1>
-            Hi, I'm<br />
-            Brian.
-          </h1>
-        </div>
-
-        <div className="blog">
-          <h2>Blog</h2>
-          <p>Coming soon!</p>
-        </div>
+        <h1>
+          Hi, I'm<br />
+          Brian.
+        </h1>
       </div>
     );
   }
