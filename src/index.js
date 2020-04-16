c707136c8165c9bf5865a6b8c58c35db5888f734
Brian Strauch <bstrauch24@gmail.com>
2020-04-15 23:09:55 -0500

    Added intro

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          Hi, I'm<br />
-          Brian.
-        </h1>
+        Hi, I'm Brian! I study Computer Science at the <a href="https://illinois.edu/">University of Illinois</a>.
+        I'm passionate about open source software and sustainability.
+        I've interned at <a href="https://www.cmegroup.com/">CME Group</a> and <a href="https://www.facebook.com/">Facebook</a>, and I'm headed to <a href="https://www.confluent.io/">Confluent</a> next!
+        I directed the systems team for <a href="https://2020.hackillinois.org/">HackIllinois</a>, an open source focused hackathon.
+        I spend nearly all of my free time swimming, biking, or running.
       </div>
     );
   }
