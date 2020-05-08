76cf72c86f1040191a9f8ae45586787415e514f3
= <bstrauch24@gmail.com>
2020-05-08 10:17:47 -0500

    Updated bio

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ class App extends React.Component {
     return (
       <div className="App">
         Hi, I'm Brian! I study Computer Science at the <a href="https://illinois.edu/">University of Illinois</a>.
-        I'm passionate about open source software and sustainability.
-        I've interned at <a href="https://www.cmegroup.com/">CME Group</a> and <a href="https://www.facebook.com/">Facebook</a>, and I'm headed to <a href="https://www.confluent.io/">Confluent</a> next!
-        I directed the systems team for <a href="https://2020.hackillinois.org/">HackIllinois</a>, an open source focused hackathon.
-        I spend nearly all of my free time swimming, biking, or running.
+        I'm interested in open source software, distributed computing, and digital sovereignty.
+        In the past, I've interned at <a href="https://www.cmegroup.com/">CME Group</a> and <a href="https://www.facebook.com/">Facebook</a>, and I'm headed to <a href="https://www.confluent.io/">Confluent</a> next!
+        In 2020, I directed the systems team for <a href="https://2020.hackillinois.org/">HackIllinois</a>, an open source focused hackathon.
+        You can usually find me swimming, biking, or running.
       </div>
     );
   }
