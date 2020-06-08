0bc772107b122db7b7d16a1c227decbed9f9d164
Brian Strauch <bstrauch24@gmail.com>
2020-06-07 20:35:36 -0500

    Random point generator

--- src/index.js
+++ src/index.js
@@ -21,25 +21,78 @@ class Random extends React.Component {
     super(props);
 
     this.state = {
-      src: undefined
+      mode: 'bike',
+      dist: 10,
+
+      src: { lat: 40.113748, lon: -88.22925 },
+      dst: { lat: undefined, lon: undefined },
     };
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(position => {
-      let { latitude, longitude } = position.coords;
-      this.setState({ src: { lat: latitude, lon: longitude } });
-    }, err => {
-      this.setState({ src: { lat: err.code, lon: '' } });
+    this.update();
+  }
+
+  changeMode = event => {
+    this.setState({ mode: event.target.value }, () => {
+      this.update();
     });
   }
 
+  changeDist = event => {
+    this.setState({ dist: event.target.value }, () => {
+      this.update();
+    });
+  }
+
+  update = () => {
+    let { dist, src } = this.state;
+
+    let p = Math.random();
+    let dx = (Math.random() < 0.5 ? 1 : -1) * dist * p;
+    let dy = (Math.random() < 0.5 ? 1 : -1) * dist * (1 - p);
+
+    const EARTH_RADIUS = 3959;
+    let dLat = this.radToDeg(dy / EARTH_RADIUS);
+    let dLon = this.radToDeg(dx / EARTH_RADIUS);
+
+    let dst = {
+      lat: src.lat + dLat,
+      lon: src.lon + dLon,
+    };
+    this.setState({ dst });
+  }
+
+  radToDeg = rad => {
+    return rad * 180 / Math.PI;
+  }
+
   render() {
-    const { src } = this.state;
+    const { mode, dist, src, dst } = this.state;
+
+    let modeId = (mode === 'bike' ? 1 : 2);
+    let url = `https://www.google.com/maps/dir/${src.lat},${src.lon}/${dst.lat},${dst.lon}/data=4m2!4m1!3e${modeId}`;
 
     return (
       <div className="Random">
-        { src && <div>Your location: <span>{src.lat}, {src.lon}</span></div> }
+        I want to
+
+        <select onChange={this.changeMode}>
+          <option value="bike" selected={mode === 'bike'}>bike</option>
+          <option value="run" selected={mode === 'run'}>run</option>
+        </select>
+
+        <input
+          type="number"
+          min="0"
+          max="100"
+          defaultValue={dist}
+          onChange={this.changeDist}
+        />
+
+        miles.
+
+        { url && <button onClick={() => window.open(url, '_blank')}>Go!</button> }
       </div>
     );
   }
