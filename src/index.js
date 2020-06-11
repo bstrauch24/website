50495911e9e23c6fb16435e431daca98f854e5fa
Brian Strauch <bstrauch24@gmail.com>
2020-06-10 21:30:51 -0500

    Start anywhere option

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ function Home() {
   return (
     <div className="Home">
       Hi, I'm Brian. I study Computer Science at the <a href="https://illinois.edu/">University of Illinois</a>.
-      I'm interested in open source software, distributed computing, and digital sovereignty.
+      I'm interested in open source software and distributed computing.
       In the past, I've interned at <a href="https://www.cmegroup.com/">CME Group</a> and <a href="https://www.facebook.com/">Facebook</a>, and I'm currently working remotely at <a href="https://www.confluent.io/">Confluent</a>!
-      In 2020, I directed the systems team for <a href="https://2020.hackillinois.org/">HackIllinois</a>, an open source focused hackathon.
-      You can usually find me swimming, biking, or running.
+      Last year, I directed the systems team for <a href="https://2020.hackillinois.org/">HackIllinois</a>, an open source focused hackathon.
+      In my spare time I do a lot of <a href="/random">running and biking</a>.
     </div>
   );
 }
@@ -23,31 +23,44 @@ class Random extends React.Component {
     this.state = {
       mode: 'bike',
       dist: 10,
-
-      src: { lat: 40.113748, lon: -88.22925 },
-      dst: { lat: undefined, lon: undefined },
+      addr: '610 E Stoughton St, Champaign, IL',
+      save: [undefined, undefined], // [addr, src]
     };
   }
 
-  componentDidMount() {
-    this.update();
-  }
-
   changeMode = event => {
-    this.setState({ mode: event.target.value }, () => {
-      this.update();
-    });
+    this.setState({ mode: event.target.value });
   }
 
   changeDist = event => {
-    this.setState({ dist: event.target.value }, () => {
-      this.update();
+    this.setState({ dist: event.target.value });
+  }
+
+  changeAddr = event => {
+    this.setState({ addr: event.target.value });
+  }
+
+  go = () => {
+    const { addr, save } = this.state;
+
+    if (save[0] === addr) {
+      this.generate(save[1]);
+      return;
+    }
+
+    fetch(`https://nominatim.openstreetmap.org/search?q=${addr}&format=json`).then(res => res.json()).then(res => {
+      let src = {
+        lat: Number(res[0]['lat']),
+        lon: Number(res[0]['lon']),
+      };
+      this.setState({ save: [addr, src] });
+      this.generate(src);
     });
   }
 
-  update = () => {
-    let { dist, src } = this.state;
-
+  generate = src => {
+    const { mode, dist, addr } = this.state;
+    
     let p = Math.random();
     let dx = (Math.random() < 0.5 ? 1 : -1) * dist * p;
     let dy = (Math.random() < 0.5 ? 1 : -1) * dist * (1 - p);
@@ -60,7 +73,9 @@ class Random extends React.Component {
       lat: src.lat + dLat,
       lon: src.lon + dLon,
     };
-    this.setState({ dst });
+
+    let modeId = (mode === 'bike' ? 1 : 2);
+    window.open(`https://www.google.com/maps/dir/${addr}/${dst.lat},${dst.lon}/data=4m2!4m1!3e${modeId}`, '_blank');
   }
 
   radToDeg = rad => {
@@ -68,31 +83,34 @@ class Random extends React.Component {
   }
 
   render() {
-    const { mode, dist, src, dst } = this.state;
-
-    let modeId = (mode === 'bike' ? 1 : 2);
-    let url = `https://www.google.com/maps/dir/${src.lat},${src.lon}/${dst.lat},${dst.lon}/data=4m2!4m1!3e${modeId}`;
+    let { mode, dist, addr } = this.state;
 
     return (
       <div className="Random">
         I want to
 
-        <select onChange={this.changeMode}>
-          <option value="bike" selected={mode === 'bike'}>bike</option>
-          <option value="run" selected={mode === 'run'}>run</option>
+        <select defaultValue={mode} onChange={this.changeMode}>
+          <option value="bike">bike</option>
+          <option value="run">run</option>
         </select>
 
         <input
+          defaultValue={dist}
+          onChange={this.changeDist}
           type="number"
           min="0"
           max="100"
-          defaultValue={dist}
-          onChange={this.changeDist}
         />
 
-        miles.
+        miles, starting at
 
-        { url && <button onClick={() => window.open(url, '_blank')}>Go!</button> }
+        <input
+          id="addr"
+          defaultValue={addr}
+          onChange={this.changeAddr}
+        />
+
+        <button onClick={this.go}>Go!</button>
       </div>
     );
   }
