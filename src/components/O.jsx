df90cb292eb51d50b2d0341bba5d2b2db2cc1f49
Brian Strauch <bstrauch24@gmail.com>
2020-12-20 19:46:23 -0600

    move multiplayer games to other repo

--- src/components/O.jsx
+++
@@ -1,7 +0,0 @@
-export default function O({color}) {
-  return (
-    <svg viewBox="0 0 100 100">
-      <circle cx="50" cy="50" r="30" stroke={color} strokeWidth="4" fill="white" />
-    </svg>
-  )
-}
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import 'katex/dist/katex.min.css';
 
-import Home from './pages/Home';
-import Learn from './pages/Learn';
-import TicTacToe from './pages/TicTacToe';
 import './style.css';
 
 const root = (
-  <BrowserRouter>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/learn" component={Learn} />
-    <Route path="/tic-tac-toe" component={TicTacToe} />
-  </BrowserRouter>
+  <div>
+    Hi, I'm Brian. I study Computer Science at the <a href="https://illinois.edu/">University of Illinois</a>.
+    I'm interested in open source software, cryptography, and distributed computing.
+    In the past, I've interned at <a href="https://www.cmegroup.com/">CME Group</a>, <a href="https://www.facebook.com/">Facebook</a>, and <a href="https://www.confluent.io/">Confluent</a>.
+    Last year, I directed the systems team for <a href="https://2020.hackillinois.org/">HackIllinois</a>, an open source focused hackathon.
+    In my spare time I do a lot of <a href="https://www.strava.com/athletes/brianstrauch">swimming, biking, and running</a>!
+  </div>
 );
 
 ReactDOM.render(root, document.getElementById('root'));
